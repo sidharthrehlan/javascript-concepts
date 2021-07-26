5bc215a2d415d4f96fdb9ca0dda0af08a2dec34b
sidharth <sidharth@nagarro.com>
2021-07-26 16:08:50 +0530

    adding keys for https

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function () {
+    navigator.serviceWorker.register("/sw.js").catch(function (error) {
+      console.log("Service worker registration failed:", error);
+    });
+  });
+}
