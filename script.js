4a2ab9152a3330a8f7724790b832349cb7686da6
sidharthrehlan <sidharth.rehlan@olx.com>
2022-03-25 16:04:50 +0530

    feat: add array.map prototype

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+let sample = [1, 2, 3, 4, 5];
 
+Array.prototype.map1 = function (cb) {
+  let newArr = [];
+  let oldArr = this;
+  for (let i = 0; i < oldArr.length; i++) {
+    let newItem = cb(oldArr[i], i, oldArr, this);
+    newArr.push(newItem);
+  }
+  return newArr;
+};
+
+let sample2 = sample.map1((item) => {
+  return item * 2;
+});
+
+console.log(sample2);
