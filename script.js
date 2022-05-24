00920de53468819bcd01f2a3decf07c0c1456a3f
sidharthrehlan <sidharth.rehlan@olx.com>
2022-05-24 18:47:51 +0530

    cookie add and delete

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-
+const getCookie = (name) => {
+  return document.cookie.split(';').some((c) => {
+    return c.trim().startsWith(name + '=');
+  });
+};
+const deleteCookie = (name, path, domain) => {
+  if (getCookie(name)) {
+    document.cookie =
+      name +
+      '=' +
+      (path ? ';path=' + path : '') +
+      (domain ? ';domain=' + domain : '') +
+      ';expires=Thu, 01 Jan 1970 00:00:01 GMT';
+  }
+};
+document.cookie = 'xyz=abc;';
+document.cookie = 'abc=abc;';
+document.cookie = 'foo=abc;';
+document.cookie = 'bar=abc;';
+//deleteCookie('foo');
